e1220bb5d106892b9db464996ebeb7c1647fe202
Sabio <jasmine.sabio@gmail.com>
2022-02-05 09:15:44 +0800

    added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+
+    let txn = await nftContract.makeNFT()
+    await txn.wait()
+    console.log("Minted NFT #1")
+
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
