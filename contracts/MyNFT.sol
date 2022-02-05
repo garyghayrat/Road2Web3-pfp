d85e8e7f4f34b1ef48899ccb2e9fc05b486f1757
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-02-04 21:29:02 -0500

    small comment test

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -12,7 +12,7 @@ contract MyNFT is ERC721URIStorage {
     
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
+//comments
     string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
     
     string[] firstWords = ["Everything But The ", "5 Cheese ", "Dark Chocolate ", "Asian Inspired ", "Crispy, Crunchy ", "Gluten Free ", "Unexpectd ", "A Midsummer Night's ", "Pumpkin Spiced ", "Triple Ginger ", "Candy Cane ", "Double Fried ", "Ube Flavored ", "Chili & Lime Flavored "];
